66f9279f408149b7787924aebc0266dc3eef50ed
Sourav1011 <souravsaini417@gmail.com>
2024-11-10 22:51:28 +0530

    using react

---
+++ Sourav/fee-eval/src/App.jsx
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>Contact Us</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
